4828a828d14589b5759e08eb88cadf0bc478e34c
Vishal9098 <vishalsilawat6@gmail.com>
2026-04-16 17:39:38 +0530

    p

--- utkarsh_cleaning/urls.py
+++ utkarsh_cleaning/urls.py
@@ -1,7 +1,3 @@
-
-
-
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -12,5 +8,8 @@ urlpatterns = [
     path('', include('store.urls')),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])  # ← YE ADD KARO
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
