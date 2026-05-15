ab22e2090915551a50cef501392e708b94e6e6d3
Vishal9098 <vishalsilawat6@gmail.com>
2026-05-15 11:49:03 +0530

    r

--- store/models.py
+++ store/models.py
@@ -226,7 +226,7 @@ class OrderTracking(models.Model):
                                 help_text="e.g. Team raste mein hai")
     updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+  
     class Meta:
         ordering = ['-created_at']
 
